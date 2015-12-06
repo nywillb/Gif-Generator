9cdd422d40da03dad9590bcd5fb99d36bc97b440
nywillb <william@barkoffusa.com>
2015-12-06 18:40:22 +0000

    Make parse

--- application_controller.rb
+++ application_controller.rb
@@ -6,9 +6,13 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
-  
-  post '/translate' do 
-    
+
+  post '/translate' do
+    puts params[:text]
+    text = params[:text].split(" ")
+    text.each do |value|
+      puts value
+    end
   end
 end
 
